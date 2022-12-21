4ec8ab7289f106cc791090f7d88cb46b1277f8ed
JwaSangHeon <whk4051@naver.com>
2022-12-21 23:16:09 +0900

    다시 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React from "react";
-import Home from "./pages/Home";
-import NewPost from "./pages/NewPost";
-import { Routes, Route } from "react-router-dom";
-import Post from "./pages/Post";
-import Header from "./components/Header";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/newpost" element={<NewPost />} />
-        <Route path="/post" element={<Post />} />
-      </Routes>
-    </>
-  );
+  return <>1</>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 import App from "./App";
-import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <RecoilRoot>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </RecoilRoot>
-);
+root.render(<App />);
